336a9aaf4a4d45420589e8973a615303221cd4e8
Clae School VM <claemiller@gmail.com>
2025-12-22 12:55:38 -0600

    Added for each loop and echos

--- mpanup.sh
+++ mpanup.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-#
+# Be sure to add this current filepath to matedisplayrule.rules 
+# Also make sure matedisplayrule.rules is in /etc/udex/rules.d/
+
+echo "mpanup starting"
+
+# Gets Displays
+displays=$(xrandr --listmonitors)
+
+# Creates a foreach loop
+for display in "{displays[@]}"; do
+    echo "Starting  $display"
+    # Clears panels
+    
+    # Sets up panels
+
+    # Creates panels
+
+    # Adds widgets
+
+    echo "Completed $display"
+done
+echo "mpanup complete"
