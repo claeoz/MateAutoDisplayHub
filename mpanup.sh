01d977ceb2bb507a625009a5dfbba2ce26a1f688
Clae School VM <claemiller@gmail.com>
2025-12-24 09:44:46 -0600

    checks if theres 1 display and if so just does the reset command

--- mpanup.sh
+++ mpanup.sh
@@ -8,17 +8,24 @@ echo "mpanup starting"
 # Gets Displays
 displays=$(xrandr --listmonitors)
 
-# Creates a foreach loop
-for display in "{displays[@]}"; do
-    echo "Starting  $display"
-    # Clears panels
-    
-    # Sets up panels
+#checks if theres only one display
+if [[ ${#displays[@]} -eq 1 ]]
+    #Sets panels back to default
+    mate-panel --reset
 
-    # Creates panels
+else
+    # Creates a foreach loop
+    for display in "{displays[@]}"; do
+        echo "Starting  $display"
+        # Clears panels
+        
+        # Sets up panels
 
-    # Adds widgets
+        # Creates panels
 
-    echo "Completed $display"
-done
+        # Adds widgets
+
+        echo "Completed $display"
+    done
+fi
 echo "mpanup complete"
